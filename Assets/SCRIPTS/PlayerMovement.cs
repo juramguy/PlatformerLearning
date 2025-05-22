784f399450357245a939454257562232a601a312
juramguy <jeremy.muelleryt@gmail.com>
2025-05-22 12:09:22 +0200

    PlayerMovementScript
    Added Playermovement script and froze rotation z on rigidbody 2d on the player. player can now move left and right.
    v1.

--- Assets/SCRIPTS/PlayerMovement.cs
+++ Assets/SCRIPTS/PlayerMovement.cs
@@ -1,16 +1,34 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private float horizontal;
+    public float moveSpeed = 10f;
+
+    private Rigidbody2D rb;
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetButton("Horizontal"))
+        {
+            rb.linearVelocity = new Vector2(horizontal * moveSpeed, rb.linearVelocityY) * Time.deltaTime;
+        }
+
+
     }
+
+
 }
