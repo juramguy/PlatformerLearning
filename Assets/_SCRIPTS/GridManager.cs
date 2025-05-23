9063f9c67a7ed6685df9fe87bf681b6213cb2d3a
juramguy <jeremy.muelleryt@gmail.com>
2025-05-23 07:21:12 +0200

    TileManager and Tiles
    Added Tilemanager, so that a Grid of tiles will spawn and added a onmousedown event that the tiles can get destroyed when you click them.

---
+++ Assets/_SCRIPTS/GridManager.cs
@@ -0,0 +1,34 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridManager : MonoBehaviour
+{
+
+    [SerializeField] private int _width, _height;
+
+    [SerializeField] private Tile _tilePrefab;
+
+    [SerializeField] private Transform _camera;
+
+    private void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x,y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+            }
+        }
+
+        _camera.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
+
+    }
+
+}
